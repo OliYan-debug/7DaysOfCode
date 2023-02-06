a6458e66c6e1591fdbc405a689974f9a9a03504e
Yan Oliveira <77648737+OliYan-debug@users.noreply.github.com>
2023-02-06 10:43:57 -0300

    Day One - Request para o imdb api retornando um json com 250 filmes

---
+++ README.md
@@ -0,0 +1,17 @@
+# IMDB TOP 250
+
+## Day one
+
+- Criar um Request para o IMDB e retornar os tops 250 filmes do momento
+
+
+
+
+
+
+
+
+
+## Dependências 
+
+- OkHttp3
--- src/main/java/imbd/top250/Main.java
+++ src/main/java/imbd/top250/Main.java
@@ -1,7 +1,31 @@
 package imbd.top250;
 
+import imbd.top250.env.Credentials;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static OkHttpClient client = new OkHttpClient();
+
+    public static String run (String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try(Response response = client.newCall(request).execute()){
+            return response.body().string();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String url = "https://imdb-api.com/en/API/Top250Movies/" + Credentials.getApiKey();
+        String jsonString = run(url);
+        System.out.println(jsonString);
     }
 }
